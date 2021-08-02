625e0a3d84ca4037673086f0bc27c69c0c4d2ab7
andres857 <andres857@gmail.com>
2021-08-02 13:55:20 +0000

    add design on the channel and room tab

--- web-ui/src/components/sala.vue
+++ web-ui/src/components/sala.vue
@@ -3,19 +3,17 @@
     img-src="https://mediawebwordpress.nyc3.digitaloceanspaces.com/Imbanaco/salasespera/salasespera/Sotano4-radioterapia.jpeg"
     img-alt="Image"
     img-top
-    style="max-width: 18rem;"
+    class="MD_cont_imgCard"
     >
     <template #header>
-      <h4 class="mb-0">Sotano 4</h4>
+      <h4 class="mb-0 MD_TTCard">Sotano 4</h4>
     </template>
-            <b-row align-v="center" align-h="center">
-                <b-col cols="1">
-                    <b-icon icon="circle-fill" variant="success"></b-icon>
-                </b-col>
-                <b-col cols="6">
+            <b-row class="MD_card">
+                <b-col class="MD_contCardInfo">
+                    <b-icon icon="circle-fill" variant="success" font-scale="0.7"></b-icon>
                     <span>Radioterapia 1</span>
                 </b-col>
-                <b-col cols="1">
+                <b-col cols="1" class="MD_contPuntos">
                     <b-dropdown size="sm" variant="link" toggle-class="text-decoration-none" no-caret>
                         <template #button-content>
                             <b-icon icon="three-dots-vertical" aria-hidden="true" ></b-icon>
@@ -27,14 +25,13 @@
             </b-row>
             <hr>
 
-            <b-row align-v="center" align-h="center">
-                <b-col cols="1">
-                    <b-icon icon="circle-fill" variant="success"></b-icon>
-                </b-col>
-                <b-col cols="6">
+            <b-row class="MD_card">
+
+                <b-col class="MD_contCardInfo">
+                    <b-icon icon="circle-fill" variant="success" font-scale="0.7"></b-icon>
                     <span>Radioterapia 2</span>
                 </b-col>
-                <b-col cols="1">
+                <b-col cols="1" class="MD_contPuntos">
                     <b-dropdown size="sm" variant="link" toggle-class="text-decoration-none" no-caret>
                         <template #button-content>
                             <b-icon icon="three-dots-vertical" aria-hidden="true" ></b-icon>
@@ -46,14 +43,12 @@
             </b-row>
             <hr>
 
-            <b-row align-v="center" align-h="center">
-                <b-col cols="1">
-                    <b-icon icon="circle-fill" variant="success"></b-icon>
-                </b-col>
-                <b-col cols="6">
+            <b-row class="MD_card">
+                <b-col class="MD_contCardInfo">
+                    <b-icon icon="circle-fill" variant="success" font-scale="0.7"></b-icon>
                     <span> Medicina Nuclear </span>
                 </b-col>
-                <b-col cols="1">
+                <b-col cols="1" class="MD_contPuntos">
                     <b-dropdown size="sm" variant="link" toggle-class="text-decoration-none" no-caret>
                         <template #button-content>
                             <b-icon icon="three-dots-vertical" aria-hidden="true" ></b-icon>
@@ -73,5 +68,69 @@ export default {
 </script>
 
 <style scoped>
+.MD_cont_imgCard img {
+    height: 130px;
+    width: max-content;
+    margin: 0 auto;
+    border-radius: 5px 5px 0 0;
+}
 
+.MD_cont_imgCard {
+    max-width: 96%;
+    margin: 0 auto;
+    border-radius: 5px;
+}
+
+.MD_cont_imgCard .card-header {
+    padding: 4px 16px;
+    background: #00B2A9;
+}
+
+.MD_cont_imgCard .card-header h4{
+    font-size: 20px;
+    font-weight: 700;
+    color: #ffffff;
+}
+
+.MD_card{
+    margin: 0;
+    justify-content: space-between;
+}
+
+.MD_contCardInfo{
+    padding: 0;
+}
+
+.MD_contCardInfo svg{
+    margin-right: 10px;
+}
+
+div.card-body {
+    padding: 11px 19px;
+}
+
+.MD_contPuntos{
+    padding: 0;
+    width: 28px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 60px;
+    background: #E6F1F1;
+    transition: all 0.4s;
+}
+
+.MD_contPuntos svg.b-icon{
+    fill: #00B2A9;
+    transition: all 0.4s;
+}
+
+.MD_contPuntos:hover{
+    background: #00B2A9;
+}
+
+.MD_contPuntos:hover svg.b-icon{
+    fill: #ffffff;
+}
 </style>
--- web-ui/src/views/Canal.vue
+++ web-ui/src/views/Canal.vue
@@ -1,43 +1,45 @@
 <template>
-  <b-container>
-    <b-row   class="mb-5 mt-5"  align-h="center">
-      <b-col cols="8">
+  <b-container class="MDContainer">
+    <b-row   class="mb-5 mt-5 MD_contAllTT_canal"  align-h="left">
+      <div class="TT_indentificador_canal"></div>
+      <b-col cols="8" class="MD_contTT_canal">
         <h2>Administrador de Canales de Windows Channel</h2>
       </b-col>
     </b-row>
 
     <!-- Reproductor y info -->
-    <b-row align-h="around" >
+    <div class="MD_cont_alterAll">
+      <alternadora sala="Alternadora" tv=""/>
+    </div>
+    <!-- <b-row align-h="around" >
 
       <b-col cols="7">
-        <alternadora sala="Alternadora" tv=""/>
       </b-col>
 
-       <b-col cols="5" class="channels ">
-         <div>
-           <h4 class="m-3 text-center">Canales</h4>
-         </div>
 
-        <div class="ml-4">
-          <videoplayerwchannel :options="videoOptions"/>
+    </b-row> -->
 
-          <b-button class="mt-2 pl-3" pill variant="success">Imbanaco tv 
-            <b-icon class="ml-1" icon="circle-fill" animation="throb" font-scale="1.1"></b-icon>
-          </b-button>
-
-        </div >
-        <div class="mt-3 ml-4">
-          <videoplayerwcomercial :options="videoOptionsComercial"/>
-
-          <b-button disabled  class="mt-2 pl-3" pill variant="danger">Caracol
-            <b-icon class="ml-1 " icon="circle-fill"  font-scale="1.1"></b-icon>
-          </b-button>
-
+    <b-col cols="5" class="channels MD_contAll_video">
+        <div>
+          <h4 class="text-center MD_contTT_canales">CANALES</h4>
         </div>
-      </b-col>
 
-    </b-row>
+      <div class="MD_contVideos">
+        <b-button class="mt-2 pl-3 MD_videos_TT" pill variant="success">
+          <b-icon class="mr-1" icon="circle-fill" animation="throb" font-scale="1.1"></b-icon>Imbanaco tv 
+        </b-button>
 
+        <videoplayerwchannel :options="videoOptions"/>
+      </div >
+
+      <div class="MD_contVideos">
+        <b-button disabled  class="mt-2 pl-3 MD_videos_TT" pill variant="danger">
+          <b-icon class="mr-1 " icon="circle-fill"  font-scale="1.1"></b-icon>Caracol
+        </b-button>
+
+        <videoplayerwcomercial :options="videoOptionsComercial"/>
+      </div>
+    </b-col>
     
   </b-container>
 </template>
@@ -61,8 +63,8 @@ export default {
       videoOptions: { 
         autoplay: false, 
         controls: true,
-        width:380,
-        height:214, 
+        width:273,
+        height:154, 
         poster:'https://mediawebwordpress.nyc3.cdn.digitaloceanspaces.com/imbanaco/bgwc.png',
         sources: [ 
           { 
@@ -74,8 +76,8 @@ export default {
       videoOptionsComercial: { 
         autoplay: false, 
         controls: true,
-        width:380,
-        height:214, 
+        width:273,
+        height:154, 
         poster:'https://mediawebwordpress.nyc3.cdn.digitaloceanspaces.com/imbanaco/bgcomercial.png',
         sources: [ 
           { 
@@ -88,13 +90,95 @@ export default {
     }
   },
 }
+
+let $body = document.querySelector("body");
+
+$body.style.backgroundColor = "#F4F8F8";
 </script>
 
 <style  scoped>
+*{
+  font-family: Calibri,Candara,Segoe,Segoe UI,Optima,Arial,sans-serif; 
+}
+
+
 /* #fafafa */
 .channels{
     background-color:#fafafa;
     /* E03E52 rojo */
 }
+
+/********* CANALES *********/
+.MDContainer{
+  position: relative;
+  min-height: 100vh;
+}
+
+div.MD_contAllTT_canal {
+  margin-top: 34px !important;
+  margin-bottom: 30px !important;
+  align-items: center;
+}
+
+.TT_indentificador_canal{
+  background: #00B2A9;
+  height: 25px;
+  width: 7px;
+  border-radius: 1px;
+}
+
+.MD_contTT_canal{
+  padding: 0px;
+}
+
+.MD_contTT_canal h2 {
+    margin-left: 8px;
+    font-weight: 700;
+    font-size: 35px;
+}
+
+.MD_contAll_video {
+  position: absolute;
+  width: 29.4%;
+  background: #ffffff;
+  margin-top: 176px;
+  /* margin-left: auto; */
+  margin-right: 56px;
+  padding: 0;
+  border-radius: 16px;
+  overflow: hidden;
+  padding-bottom: 8px;
+  box-shadow: 0px 8px 22px 0px rgb(194 198 207 / 50%);
+  top: 0;
+  right: 0;
+}
+
+.MD_contVideos div, .MD_contVideos {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0 auto;
+  margin-bottom: 7px;
+}
+
+.MD_videos_TT{
+  border-radius: 0!important;
+  max-width: 273px;
+  margin: 0 auto;
+  width: 100%;
+  text-align: left;
+  padding: 0;
+}
+
+.MD_contTT_canales{
+  font-size: 18px!important;
+  font-weight: 700;
+  letter-spacing: 3px;
+  color: #ffffff;
+  background: #00B2A9;
+  padding: 17px 0px;
+  margin-bottom: 20px;
+}
+/********* EndCANALES *********/
 
 </style>
